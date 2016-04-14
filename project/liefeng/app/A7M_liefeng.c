a0adfa75b458d4d0f49ba16871b605ee1d6a0a9b
bkseastone <bkseastone@gmail.com>
2016-04-14 19:06:27 +0800

    add OV7725_eagle driver;

--- project/liefeng/app/A7M_liefeng.c
+++ project/liefeng/app/A7M_liefeng.c
@@ -7,17 +7,16 @@ void main (void)
 	//if(ftm_pwm_init(&Pwm0_A4, 54, 3000)==SUCCESS);
 	
 	//if(ic_init(&ic2_A11)==SUCCESS);
-	adc_init(&Adc0_0P1, 1000000);
+	//adc_init(&Adc0_0P1, 1000000);
 	
-	//ov7620_dma_start();
+	ov7725_dma_start();
 	
 	while(1)
 	{
 		
-		//if(Is_DispPhoto==1) OV_display();
-		//printf("Freq2 = %dHz. \nFreq3 = %dHz. \n", Freq2, Freq3);
-		//printf("nothing\n");
-		
-		;
+		if(Is_DispPhoto==1)
+		{
+			OV_display();
+		}
 	} 
 }
--- project/liefeng/app/main.h
+++ project/liefeng/app/main.h
@@ -4,20 +4,8 @@
 #include "time.h"
 #include "UART.h"
 #include "ADCConfig.h"
-#include "OV7620.h"
+#include "OV7725_eagle.h"
 #include "Capture.h"
-
-/**
- * @brief Macro to access a single bit of SRAM (bit band region
- *        0x20000000 to 0x200FFFFF) using the bit-band alias region access.
- * @param Line Picture`s lines to access, ranging in [0~inf].
- * @param Pix Lines` pixels to access, ranging in [0~OV_P-1].
- * @return Value of the targeted bit in the bit band region.
- */
-#define SRAM_U_BASE						0x20000000u
-#define OV_binary_BASE					SRAM_U_BASE
-#define OV_pics_LineNum(Lines)			(4u*((uint32_t)Lines*OV_P))
-#define OV_binary_data(Line,Pix) (*((uint32_t volatile*)(0x22000000u + (OV_pics_LineNum(Line)) + (4u*(uint32_t)Pix))))
 
 // constant for initializing peripherals
 mypwm Pwm0_A4={FTM0, FTM_Ch1, PTA4};  // 舵机
@@ -30,7 +18,6 @@ myic ic2_A11={FTM2, FTM_Ch1, PTA11, &ic3_isr};
 
 // Global variables (online)
 uint8 Is_DispPhoto; //图像采集完成标志
-uint8 Pix_Data[PHOTO_SIZE]; //采集320行 240列的图像数据  
 uint32 Freq2 = 0; 
 uint32 Freq3 = 0;
 
